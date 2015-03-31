513d43bc82ec6307c7df07695e09532d60ba80cd
Stefan Ilie <ilie.stefan92@yahoo.com>
2015-03-31 17:46:30 +0300

    add twitter search. needs debug

--- search/views.py
+++ search/views.py
@@ -3,17 +3,39 @@ from django.shortcuts import render_to_response
 from .forms import SearchForm
 from .models import Search
 
-def search(request):
-	#add if post or if get and call 
-	#getTweets with searchfield content
+#TODO: add new page to send results of query to.
+#name it, i dunno, results.html
+def home(request):
 	form=SearchForm(request.POST or None)
 	context = {"form": form}
 	template="search.html"
 	return render(request, template, context)
 
-def home(request):
-	context={'homepage': homepage, 'tweets': getTweets()}
-	return render_to_response('home.html', context, context_instance=RequestContext(request))
+
+def search(request):
+	#add if post or if get and call 
+	#getTweets with searchfield content
+	if request.method == 'GET':
+		form = SearchForm()
+	elif request.method == 'POST':
+		form = SearchForm(request.POST)
+		arrTweets=''
+		if form.is_valid():
+			if form.is_valid():
+				cleaned_data = form.cleaned_data
+				strTerm = cleaned_data['search']
+				arrTweets = getTweets(strTerm)
+		context = {
+			"form": form,
+			"tweets": arrTweets
+		}
+		template = "search.html"
+		return render(request, template, context)
+
+	# form=SearchForm(request.POST or None)
+	# context = {"form": form}
+	# template="search.html"
+	# return render(request, template, context)
 
 #add search field contents
 #add options for popular, recent, no. of results, 
